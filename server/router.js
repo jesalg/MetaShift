6ed55a80534a1492e472f64c9b7e658d12dafd44
Jesal Gadhia <jesal7@gmail.com>
2017-10-31 23:38:37 -0700

    Setting up an endpoint to serve URLs (#2)

--- server/router.js
+++ server/router.js
@@ -5,18 +5,21 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {  })
+  res.render('index', {})
 })
 
-router.get('/link', function(req, res, next) {
-  res.render('app', {  })
+router.get('/new', function(req, res, next) {
+  res.render('app', {})
 })
 
-router.get('/link/:edit_hash', function(req, res, next) {
-  res.render('app', {  })
+router.get('/edit/:edit_hash', function(req, res, next) {
+  models.Link.findOne({ where: {editHash: req.params.edit_hash} }).then(link => {
+  }).then(() => {
+    res.render('app', {link: link})
+  })
 })
 
-router.post('/link', function(req, res, next) {
+router.post('/create', function(req, res, next) {
   models.Link.create({
       url: req.body.link,
       meta: req.body.meta,
@@ -27,8 +30,23 @@ router.post('/link', function(req, res, next) {
     });
 })
 
-router.patch('/link/:edit_hash', function(req, res, next) {
+router.patch('/update/:edit_hash', function(req, res, next) {
+  models.Link.findOne({ where: {editHash: req.params.edit_hash} }).then(link => {
+    link.update({
+      url: req.body.link,
+      meta: req.body.meta,
+      twitterMeta: req.body.twitterMeta,
+      facebookMeta: req.body.facebookMeta,
+    }).then(() => {
+      res.json(link.get({plain: true}));
+    })
+  })
+})
 
+router.get('/:view_hash', function(req, res, next) {
+  models.Link.findOne({ where: {viewHash: req.params.view_hash} }).then(link => {
+    res.render('show', {link: link})
+  })
 })
 
 export default router
